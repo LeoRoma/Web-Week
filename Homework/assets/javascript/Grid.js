78073ed49ad44ba96665086a44761622745ca70b
LeoRoma <xiajtn@gmail.com>
2020-08-12 14:27:08 +0100

    can get selected breed

--- Homework/assets/javascript/Grid.js
+++ Homework/assets/javascript/Grid.js
@@ -1,4 +1,4 @@
-function fetchData(){
+$(function fetchData(){
     fetch('http://api.thecatapi.com/v1/breeds?f16ad962-557b-4deb-b7ab-626cc52a45f9')
     .then(response => { 
         console.log(response)
@@ -9,14 +9,42 @@ function fetchData(){
     })
     .then(data => {
         console.log(data)
-        const html = data.map(user => {
-            return `<p>Name: ${user.name} Origin: ${user.origin}</p>`
+        const html = data.map(breed => {
+            return `<option value="${breed.id}">${breed.name}</option>`
         }).join(" ");
         console.log(html);
-        document.querySelector('.grid-item').insertAdjacentHTML('afterbegin', html) 
+        document.querySelector('.dropdown').insertAdjacentHTML('afterbegin', html) 
     }).catch(error => {
         console.log(error);
     })
-}
+});
 
-fetchData();
+function getSelectedBreed(){
+    var selectedBreed = document.getElementById("breedsList").value;
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${selectedBreed}`)
+    .then(response => { 
+        console.log(response)
+        if(!response.ok){
+            thorw ("Error");
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        const html = data.map(cat => {
+            return `<h2>Name: ${cat.breeds[0].name}<h2> 
+                    <h3>Origin: ${cat.breeds[0].origin}<h3>
+                    <p>Description: ${cat.breeds[0].description}</p>
+                    <img src="${cat.url}" width="200" height="200"/>`
+        }).join(" ");
+        console.log(html);
+        document.querySelector('.grid-item').innerHTML = html; 
+    }).catch(error => {
+        console.log(error);
+    })
+};
+
+function getSelectValue(){
+    var selectedValue = document.getElementById("breedsList").value;
+    console.log(selectedValue);
+};
