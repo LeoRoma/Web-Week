44324e0172077e828f066fb67d2453b64909a397
LeoRoma <xiajtn@gmail.com>
2020-08-16 13:00:49 +0100

    application created, navbar added

---
+++ northwind-react/src/Components/MainPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Navbar from './Navbar';
+
+class MainPage extends Component {
+    render() {
+        return(
+            <Navbar />
+         
+        )
+    } 
+};
+
+export default MainPage;
+
---
+++ northwind-react/src/Components/Navbar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
+} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    return (
+      <div id="classicformpage">
+        <Router>
+          <MDBNavbar color="transparent" dark expand="md">
+            <MDBNavbarBrand>
+              <strong className="white-text">NORTHWIND-REACT</strong>
+            </MDBNavbarBrand>
+            <MDBNavbarToggler onClick={this.toggleCollapse} />
+            <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+              <MDBNavbarNav left>
+                <MDBNavItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle nav caret>
+                      <MDBIcon icon="user" />
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu
+                      width="20%">
+                      <MDBDropdownItem href="#!"></MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink className="waves-effect waves-light" to="https://github.com/LeoRoma">
+                    <MDBIcon fab icon="github" />
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink className="waves-effect waves-light" to="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/">
+                    <MDBIcon fab icon="linkedin" />
+                  </MDBNavLink>
+                </MDBNavItem>
+
+              </MDBNavbarNav>
+              <MDBNavbarNav right>
+                <MDBNavItem active>
+                  <MDBNavLink to="#">Home</MDBNavLink>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBNavbar>
+        </Router>
+      </div>
+
+    )
+  }
+}
+
+
+export default Navbar;
